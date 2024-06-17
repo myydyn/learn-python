4949ff78309d6824ac68a590cf4c588e796a6485
Duyen <daphne.duyenhuynh@gmail.com>
2024-06-17 22:35:20 +0700

    Add new project

---
+++ 04-notepython.py
@@ -0,0 +1,19 @@
+
+from decimal import*
+
+
+age,name,gender = 18,"dap","female"
+
+age=18
+print(type (age))
+print(type (name))
+
+
+print((20)/(3))
+
+getcontext () .prec = 3
+print(Decimal(20)/Decimal(3))
+
+
+getcontext () .prec = 30
+print(Decimal(20)/Decimal(3))
