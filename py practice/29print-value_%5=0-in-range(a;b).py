05649b8469da2f2f037a27673086fb5b85d13fd0
Duyen <daphne.duyenhuynh@gmail.com>
2024-07-22 22:59:18 +0700

    change not much

--- py practice/29print-value_%5=0-in-range(a;b).py
+++ py practice/29print-value_%5=0-in-range(a;b).py
@@ -10,17 +10,17 @@ if is_true:
   if a>b:
     print('a have to smaller than b')
   else:
-    lst = 0
+    count = 0 #tạo cái biến đếm này để đặt giới hạn dừng, rằng mình chỉ lấy 10 giá trị tương ứng count = 10
     for i in range(a, b+1):
       if i%5==0:
-        lst += 1
-        if lst>10:
+        count += 1
+        if count>10:
           print('\nmore than 10 values, pls cut off range')
           break
         print(i, end=' ')
     
     else:
-      if lst == 0:
+      if count == 0:
        print('do not have any value %5==0')
       else:
        print('\nDone! That is all value device for 5 left 0')
