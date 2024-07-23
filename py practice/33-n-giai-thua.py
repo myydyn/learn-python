f943da21ed0d9bdcc7e85b2c085d362d4faac1cf
Duyen <daphne.duyenhuynh@gmail.com>
2024-07-23 23:10:59 +0700

    giai thừa aka factorial, dùng hàm import math.factorial() hoặc result *= i

---
+++ py practice/33-n-giai-thua.py
@@ -0,0 +1,15 @@
+is_it = False
+try:
+    n = int(input('enter n to get n!, n= '))
+    is_it = True
+except:
+    print('invalid value')
+if is_it:
+    if n<0:
+        print('pls enter a negative value')
+    else:
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        print(result)
+
