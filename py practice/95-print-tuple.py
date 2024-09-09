63cc5b9c231b59c1a65c77ed7a81ae8fbe74b1bc
Duyen <daphne.duyenhuynh@gmail.com>
2024-09-09 14:25:40 +0700

    create tuple

---
+++ py practice/95-print-tuple.py
@@ -0,0 +1,10 @@
+# Viết chương trình khởi tạo hai tuple() bất kỳ bằng hai cách: 
+# Sử dụng cặp dấu ngoặc tròn và sử dụng Constructor tuple(). 
+# Hiển thị hai tuple đó ra màn hình.
+# vd: output: ('Kteam', 2021, True)
+#             (2021, 'Kteam', False)
+
+a = ('kteam', 2024, True)
+b = tuple([2021, 'kteam', False])
+print(a)
+print(b)
